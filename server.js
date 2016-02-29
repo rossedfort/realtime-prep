ea0c86d72248ff62ab6f07094c878df17b527cc6
rossedfort <rossedfort@gmail.com>
2016-02-29 08:22:17 -0700

    change port for heroku

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var app = express();
 var http = require('http').Server(app);
 const path = require('path');
 var io = require('socket.io')(http);
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
-
 app.get('/', function(req, res){
   response.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.listen(app.get('port'), function(){
+  console.log('listening on', app.get('port'));
 });
 
 io.on('connection', function(socket){
